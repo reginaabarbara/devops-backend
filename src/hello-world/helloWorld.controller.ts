f7d35bd9f64d81037e263e53696958056a3163ac
reginabarbara <reginaa_barbara@hotmail.com>
2024-11-06 18:43:08 -0300

    teste variáveis

--- src/hello-world/helloWorld.controller.ts
+++ src/hello-world/helloWorld.controller.ts
@@ -6,9 +6,9 @@ dotenv.config();
 export class HelloWorldController {
   @Get('helloWorld')
   getHelloWorld(): string {
-    // Acessando a variável de ambiente 'BRANCH_NAME'
-    const mode = process.env.MODE || 'branch desconhecida'; // Valor padrão caso a variável não esteja definida
+  
+    const mode = process.env.MODE || 'branch desconhecida'; 
     console.log('Variável mode :', process.env.mode);
-    return `Hello World! A branch atual é : `;
+    return `Hello World! A branch atual é : ${mode}`;
   }
 }
